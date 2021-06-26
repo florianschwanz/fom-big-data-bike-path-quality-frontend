d1cb6524921b6a4ca754e6f5cfeb011a848d1464
Florian Schwanz <florian.schwanz@interoberlin.de>
2021-06-27 00:16:36 +0200

    feat: add Material icon service #4

---
+++ src/app/core/ui/services/material-icon.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
+
+/**
+ * Handles Material icons
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class MaterialIconService {
+
+  /**
+   * Initializes icons
+   *
+   * @param iconRegistry icon registry
+   * @param sanitizer sanitizer
+   */
+  public initializeIcons(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('pedal_bike', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/pedal_bike_black_24dp.svg'));
+  }
+}
